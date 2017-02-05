b49d2ce18bbafc672d2efd9d67672eb3ea2a0828
QuickSand <775659148@qq.com>
2017-02-05 23:53:59 +0800

    添加监听器

--- src/com/goucraft/Listener/gouListener.java
+++ src/com/goucraft/Listener/gouListener.java
@@ -1,9 +1,21 @@
 package com.goucraft.Listener;
 
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Created by Administrator on 2017/2/5 0005.
  */
 public class gouListener implements Listener {
+    private final JavaPlugin plugin;
+    private Economy economy = null;
+
+    public gouListener(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public void setEconomy(Economy e) {
+        this.economy = e;
+    }
 }
